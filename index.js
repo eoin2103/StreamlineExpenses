2eacaeae87c9486688354d61b58a9f40fd0cea62
eoin2103 <48318648+eoin2103@users.noreply.github.com>
2021-03-19 00:07:17 +0000

    JWT auth updated

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ dotenv.config();
 mongoose.connect( process.env.DB_CONNECTION , { useNewUrlParser: true }, { useUnifiedTopology: true }, () => console.log("Connected to db"));
 
 //Import routes
-const authrouter = require('./routes/authroute');
+const authroute = require('./routes/authroute');
+const claimsroute = require('./routes/claims');
 
 //middleware
 app.use(express.json());
 
 //middleware routes
-app.use('/api/user', authrouter);
+app.use('/api/user', authroute);
+app.use('/api/claims', claimsroute);
 
 app.listen(3000, () => console.log('Server running'));
--- routes/verifyJWT.js
+++ routes/verifyJWT.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-function verifyAuth(req,res,next){
+module.exports = function(req,res,next){
     const token = req.header('auth-jwt');
     if(!token) return res.status(400).send('Access Denied User not logged in');
 
     try{
         const verify = jwt.verify(token, process.env.JTOKEN_SECRET);
         req.user = verify;
+        next();
     }catch(err){
         res.status(400).send('Error Invalid Token!!');
     }
